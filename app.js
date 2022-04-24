5676af360f64ece6c1a9ed5141444e8f553c4ad9
Ruslan O <realnaru@gmail.com>
2022-04-24 20:08:50 +0800

    Add connection and schema

--- app.js
+++ app.js
@@ -5,4 +5,13 @@ import mongoose from 'mongoose';
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 
+mongoose.connect('mongodb://localhost:27017/cacheDb');
+
+const stringSchema = new mongoose.Schema({
+    key: { type: String, unique: true, required: true },
+    data: String,
+})
+
+const RandomString = mongoose.model('String', stringSchema);
+
 app.listen(3000, () => console.log('Server started on port 3000'));
